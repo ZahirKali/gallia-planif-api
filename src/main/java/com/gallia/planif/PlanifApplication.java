da034be9e045812fa56696aefc549166bb3c218a
Zahir KALI <sofiane.kali@gmail.com>
2022-08-08 20:18:56 +0200

    first commit

---
+++ src/main/java/com/gallia/planif/PlanifApplication.java
@@ -0,0 +1,13 @@
+package com.gallia.planif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanifApplication.class, args);
+	}
+
+}
