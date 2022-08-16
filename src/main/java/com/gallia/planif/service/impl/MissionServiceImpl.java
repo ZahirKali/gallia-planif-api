b4d2baf0f98478bf174c6c28a26578c7f332ccb7
Zahir KALI <sofiane.kali@gmail.com>
2022-08-16 23:39:18 +0200

    CRUD operations OK

--- src/main/java/com/gallia/planif/service/impl/MissionServiceImpl.java
+++ src/main/java/com/gallia/planif/service/impl/MissionServiceImpl.java
@@ -5,9 +5,9 @@ import com.gallia.planif.dao.model.entity.MissionEntity;
 import com.gallia.planif.dao.model.mapper.BusinessEntityMapper;
 import com.gallia.planif.dao.repository.GenericRepository;
 import com.gallia.planif.service.AbstractGenericService;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.stereotype.Service;
 
-import java.time.Month;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
@@ -16,7 +16,7 @@ import java.time.temporal.ChronoUnit;
 public class MissionServiceImpl extends AbstractGenericService<Mission, MissionEntity> {
 
     static final int MORNING_HOUR = 6 ;
-    static final int EVENING_HOUR = 22 ;
+    static final int EVENING_HOUR = 21 ;
 
     public MissionServiceImpl(GenericRepository<MissionEntity, Long> repository, BusinessEntityMapper mapper) {
         super(repository, mapper);
@@ -26,12 +26,48 @@ public class MissionServiceImpl extends AbstractGenericService<Mission, MissionE
         return ChronoUnit.HOURS.between(mission.getStartDate(), mission.getEndDate());
     }
 
-    private long calculateNumberOfHoursBetween(Mission mission, long start, long end) {
-
-        long result = 0;
-
+    protected Pair<Long, Long> calculate(Mission mission) {
         ZonedDateTime startDate = mission.getStartDate();
         ZonedDateTime endDate = mission.getEndDate();
+        int startDateDayOfYear = startDate.getDayOfYear();
+        int endDateDayOfYear = endDate.getDayOfYear();
+        if(startDateDayOfYear == endDateDayOfYear) {
+            return calculateNightAndMorningNumberOfHoursBetweenDatesInSameDay(startDate, endDate);
+        }
+
+        long nightResult = 0;
+        long morningResult = 0;
+        int startDayOfMonth = startDate.getDayOfMonth();
+        int startDay = startDate.getDayOfYear();
+        int startYear = startDate.getYear();
+        int startMonth = startDate.getMonthValue();
+        int startMinute = startDate.getMinute();
+        ZoneId zone = startDate.getZone();
+
+        int daysNumber = startDateDayOfYear - endDateDayOfYear;
+        for(int i = 0; i < daysNumber -1; i++) {
+            // if first iteration, start hour is the real start hour else 0
+            int startHour = i == 0 ? startDate.getHour() : 0;
+            int endHour =  i == daysNumber - 1 ? endDate.getHour() : 23;
+            int endDay =
+            if(i == daysNumber - 1) {
+                endHour = endDate.getHour();
+            } else {
+                endHour = 0;
+
+            }
+
+            ZonedDateTime start = ZonedDateTime.of(startYear, startMonth, startDayOfMonth,
+                    0, startMinute, 0, 0, zone);
+        }
+
+        return null;
+    }
+
+    protected Pair<Long, Long> calculateNightAndMorningNumberOfHoursBetweenDatesInSameDay(ZonedDateTime startDate, ZonedDateTime endDate) {
+
+        long nightResult = 0;
+        long morningResult = 0;
 
         int startDayOfMonth = startDate.getDayOfMonth();
         int startDay = startDate.getDayOfYear();
@@ -42,35 +78,46 @@ public class MissionServiceImpl extends AbstractGenericService<Mission, MissionE
 
         int endDay = endDate.getDayOfYear();
 
-        // Mission start and end the same date
-        if(startDay == endDay) {
-            ZonedDateTime zeroAM =
-                    ZonedDateTime.of(startYear, startMonth, startDayOfMonth,
-                            0, startMinute, 0, 0, zone);
+        ZonedDateTime zeroAM =
+                ZonedDateTime.of(startYear, startMonth, startDayOfMonth,
+                        0, startMinute, 0, 0, zone);
 
-            ZonedDateTime sixAM =
-                    ZonedDateTime.of(startYear, startMonth, startDayOfMonth,
-                            6, startMinute, 0, 0, zone);
+        ZonedDateTime sixAM =
+                ZonedDateTime.of(startYear, startMonth, startDayOfMonth,
+                        6, startMinute, 0, 0, zone);
 
-            ZonedDateTime twentyOnePM =
-                    ZonedDateTime.of(startYear, startMonth, startDayOfMonth,
-                            21, startMinute, 0, 0, zone);
+        ZonedDateTime twentyOnePM =
+                ZonedDateTime.of(startYear, startMonth, startDayOfMonth,
+                        21, startMinute, 0, 0, zone);
 
-            ZonedDateTime midnight = zeroAM.plusDays(1);
+        ZonedDateTime midnight = zeroAM.plusDays(1);
 
-            if(startDate.isBefore(sixAM)) {
-                result = result + ChronoUnit.HOURS.between(sixAM, startDate);
-            }
-            if(endDate.isAfter(twentyOnePM)) {
-                result = result + ChronoUnit.HOURS.between(endDate, twentyOnePM);
-            }
+        // start before 6AM and finish after 21PM
+        if(isBeforeOrEqual(startDate, sixAM) && isAfterOrEqual(endDate, twentyOnePM)) {
+            morningResult = morningResult + ChronoUnit.HOURS.between(sixAM, twentyOnePM);
+            nightResult = ChronoUnit.HOURS.between(startDate, sixAM) + ChronoUnit.HOURS.between(twentyOnePM, endDate);
         }
-        // start day and end day are differents
-        else {
-
+        // start before 6AM and finish before 21PM
+        else if(isBeforeOrEqual(startDate, sixAM) && isBeforeOrEqual(endDate, twentyOnePM)) {
+            morningResult = ChronoUnit.HOURS.between(sixAM, endDate);
+            nightResult = ChronoUnit.HOURS.between(startDate, sixAM);
         }
+        // start after 6AM and finish after 21PM
+        else if(isAfterOrEqual(startDate, sixAM) && isAfterOrEqual(endDate, twentyOnePM)) {
+            morningResult = ChronoUnit.HOURS.between(startDate, twentyOnePM);
+            nightResult = ChronoUnit.HOURS.between(twentyOnePM, endDate);
+        }
+        // start after 6AM and finish before 21PM
+        else if(isAfterOrEqual(startDate, sixAM) && isBeforeOrEqual(endDate, twentyOnePM)) {
+            morningResult = ChronoUnit.HOURS.between(startDate, endDate);
+        }
+        return Pair.of(morningResult, nightResult);
+    }
 
-
-        return 0;
+    private static boolean isBeforeOrEqual(ZonedDateTime first, ZonedDateTime second) {
+        return first.isBefore(second) || first.isEqual(second);
+    }
+    private static boolean isAfterOrEqual(ZonedDateTime first, ZonedDateTime second) {
+        return first.isAfter(second) || first.isAfter(second);
     }
 }
--- src/test/java/com/gallia/planif/service/impl/MissionServiceImplTest.java
+++ src/test/java/com/gallia/planif/service/impl/MissionServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.gallia.planif.service.impl;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.Test;
 
 import java.time.ZonedDateTime;
@@ -8,12 +9,93 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MissionServiceImplTest {
 
+    /**
+     * start before 6AM and finish after 21PM
+     */
     @Test
-    void testCheckIfEveningWork() {
+    void testCalculateNightAndMorningNumberOfHoursBetweenDatesInSameDay_case1() {
         // Given
+        ZonedDateTime startDate = ZonedDateTime.parse("2022-01-10T03:00:00Z");
+        ZonedDateTime endDate = ZonedDateTime.parse("2022-01-10T22:00:00Z");
 
         // When
+        MissionServiceImpl service = new MissionServiceImpl(null, null);
+        Pair<Long, Long> result = service.calculateNightAndMorningNumberOfHoursBetweenDatesInSameDay(startDate, endDate);
 
         // Then
+        assertEquals(15, result.getLeft());
+        assertEquals(4, result.getRight());
+    }
+
+    /**
+     * start before 6AM and finish before 21PM
+     */
+    @Test
+    void testCalculateNightAndMorningNumberOfHoursBetweenDatesInSameDay_case2() {
+        // Given
+        ZonedDateTime startDate = ZonedDateTime.parse("2022-01-10T00:00:00Z");
+        ZonedDateTime endDate = ZonedDateTime.parse("2022-01-10T20:00:00Z");
+
+        // When
+        MissionServiceImpl service = new MissionServiceImpl(null, null);
+        Pair<Long, Long> result = service.calculateNightAndMorningNumberOfHoursBetweenDatesInSameDay(startDate, endDate);
+
+        // Then
+        assertEquals(14, result.getLeft());
+        assertEquals(6, result.getRight());
+    }
+
+    /**
+     * start after 6AM and finish after 21PM
+     */
+    @Test
+    void testCalculateNightAndMorningNumberOfHoursBetweenDatesInSameDay_case3() {
+        // Given
+        ZonedDateTime startDate = ZonedDateTime.parse("2022-01-10T06:00:00Z");
+        ZonedDateTime endDate = ZonedDateTime.parse("2022-01-10T22:00:00Z");
+
+        // When
+        MissionServiceImpl service = new MissionServiceImpl(null, null);
+        Pair<Long, Long> result = service.calculateNightAndMorningNumberOfHoursBetweenDatesInSameDay(startDate, endDate);
+
+        // Then
+        assertEquals(15, result.getLeft());
+        assertEquals(1, result.getRight());
+    }
+
+    /**
+     * start after 6AM and finish before 21PM
+     */
+    @Test
+    void testCalculateNightAndMorningNumberOfHoursBetweenDatesInSameDay_case4() {
+        // Given
+        ZonedDateTime startDate = ZonedDateTime.parse("2022-01-10T08:00:00Z");
+        ZonedDateTime endDate = ZonedDateTime.parse("2022-01-10T18:00:00Z");
+
+        // When
+        MissionServiceImpl service = new MissionServiceImpl(null, null);
+        Pair<Long, Long> result = service.calculateNightAndMorningNumberOfHoursBetweenDatesInSameDay(startDate, endDate);
+
+        // Then
+        assertEquals(10, result.getLeft());
+        assertEquals(0, result.getRight());
+    }
+
+    /**
+     * start after 6AM and finish before 21PM
+     */
+    @Test
+    void testCalculateNightAndMorningNumberOfHoursBetweenDatesInSameDay_case4bis() {
+        // Given
+        ZonedDateTime startDate = ZonedDateTime.parse("2022-01-10T06:00:00Z");
+        ZonedDateTime endDate = ZonedDateTime.parse("2022-01-10T21:00:00Z");
+
+        // When
+        MissionServiceImpl service = new MissionServiceImpl(null, null);
+        Pair<Long, Long> result = service.calculateNightAndMorningNumberOfHoursBetweenDatesInSameDay(startDate, endDate);
+
+        // Then
+        assertEquals(15, result.getLeft());
+        assertEquals(0, result.getRight());
     }
 }
